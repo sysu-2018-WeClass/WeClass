3f75b8f1227530432defa7937a3fd14c6ec8c87d
sysujerome <1570992296@qq.com>
2018-06-13 23:23:48 +0800

    first commit

---
+++ app/src/main/java/com/example/asus/weclass/AppDetActivity.java
@@ -0,0 +1,150 @@
+package com.example.asus.weclass;
+
+import android.app.Activity;
+
+/**
+ * Created by ASUS on 2018/6/10.
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.DataAsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
+import retrofit2.adapter.rxjava.HttpException;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class AppDetActivity extends AppCompatActivity {
+    private TextView txtShow;
+    private Button buttonCancel;
+    private ServerService service;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.application_detail);
+
+        final Bundle bd = this.getIntent().getExtras();
+//        final String uid = bd.getString("uid");
+//        final int type = bd.getInt("type");
+
+        txtShow = (TextView)findViewById(R.id.status);
+        String toshow = new String();
+        int status = bd.getInt("status");
+        switch (status) {
+            case 1: toshow = "已通过";
+                break;
+            case 2: toshow = "待审核";
+                break;
+            case 3: toshow = "未通过";
+                break;
+            default:toshow = "出错了";
+                break;
+        }
+        txtShow.setText(toshow);
+
+        txtShow = (TextView)findViewById(R.id.classroom);
+        toshow = bd.getString("classroom");
+        txtShow.setText(toshow);
+
+        txtShow = (TextView)findViewById(R.id.book_date);
+        toshow = bd.getString("BookDate").toString();
+        txtShow.setText(toshow);
+
+        txtShow = (TextView)findViewById(R.id.application_date);
+        toshow = bd.getString("applicationDate").toString();
+        txtShow.setText(toshow);
+
+        txtShow = (TextView)findViewById(R.id.begin);
+        toshow = bd.getString("begin").toString();
+        txtShow.setText(toshow);
+
+        txtShow = (TextView)findViewById(R.id.end);
+        toshow = bd.getString("end").toString();
+        txtShow.setText(toshow);
+
+        txtShow = (TextView)findViewById(R.id.statement);
+        toshow = bd.getString("statement").toString();
+        txtShow.setText(toshow);
+
+        final Button back_but = (Button) findViewById(R.id.back_but);
+        back_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        final RequestParams params = new RequestParams();
+        params.add("AID",String.valueOf(bd.getInt("aid")));
+
+        buttonCancel =(Button) findViewById(R.id.buttonCancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> params = new HashMap<>();
+                params.put("AID", String.valueOf(bd.getInt("aid")));
+                service = ServerFactory.createService("http://120.79.84.147:8000");
+                service.cancelapp(params)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Subscriber<ResponseClass1>() {
+                            @Override
+                            public void onCompleted() {
+                                System.out.print("完成传输");
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                if (e instanceof HttpException) {
+                                    HttpException exception = (HttpException)e;
+                                    int code = exception.response().code();
+                                    if (code == 404) {
+                                        Toast.makeText(AppDetActivity.this, "无相关记录", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                            }
+
+                            @Override
+                            public void onNext(ResponseClass1 github) {
+//                                Toast.makeText(AppDetActivity.this, "成功取消预约", Toast.LENGTH_SHORT).show();
+//                                Bundle bundle2 = new Bundle();
+//                                bundle2.putInt("type", bd.getInt("type"));
+//                                bundle2.putString("uid", bd.getString("uid"));
+//                                Intent intent = new Intent(AppDetActivity.this, AppListActivity.class);
+//                                intent.putExtras(bundle2);
+//                                startActivity(intent);
+                                Bundle bundle2 = new Bundle();
+                                bundle2.putInt("pos", bd.getInt("pos"));
+                                Intent intent2 = new Intent(AppDetActivity.this, MainActivity.class);
+                                intent2.putExtras(bundle2);
+                                setResult(2, intent2);
+                                finish();
+
+                            }
+                        });
+            }
+        });
+
+    }
+}
+
---
+++ app/src/main/java/com/example/asus/weclass/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.asus.weclass;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Bundle bundle1 = this.getIntent().getExtras();
+        if (bundle1 != null) {
+            final int type = bundle1.getInt("type");
+            final int uid = bundle1.getInt("uid");
+            final Button stu_view_class = (Button) findViewById(R.id.stu_view_class);
+            final Button stu_view_appoint = (Button) findViewById(R.id.stu_view_appoint);
+            final Button stu_passchange = (Button) findViewById(R.id.stu_passchange);
+            final Button man_view_class = (Button) findViewById(R.id.man_view_class);
+            final Button man_view_appoint = (Button) findViewById(R.id.man_view_appoint);
+            final Button man_passchange = (Button) findViewById(R.id.man_passchange);
+
+
+            if (type == 0) {
+                stu_view_class.setVisibility(View.INVISIBLE);
+                stu_view_appoint.setVisibility(View.INVISIBLE);
+                stu_passchange.setVisibility(View.INVISIBLE);
+                man_view_class.setVisibility(View.VISIBLE);
+                man_view_appoint.setVisibility(View.VISIBLE);
+                man_passchange.setVisibility(View.VISIBLE);
+            } else {
+                stu_view_class.setVisibility(View.VISIBLE);
+                stu_view_appoint.setVisibility(View.VISIBLE);
+                stu_passchange.setVisibility(View.VISIBLE);
+                man_view_class.setVisibility(View.INVISIBLE);
+                man_view_appoint.setVisibility(View.INVISIBLE);
+                man_passchange.setVisibility(View.INVISIBLE);
+            }
+
+
+            final Bundle bundle2 = new Bundle();
+            bundle2.putInt("type", type);
+            bundle2.putInt("uid", uid);
+
+            stu_view_class.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(MainActivity.this, ClassListActivity.class);
+                    intent.putExtras(bundle2);
+                    startActivity(intent);
+                }
+            });
+
+            stu_view_appoint.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, AppListActivity.class);
+                    intent.putExtras(bundle2);
+                    startActivity(intent);
+                }
+            });
+
+            stu_passchange.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, PassActivity.class);
+                    intent.putExtras(bundle2);
+                    startActivityForResult(intent, 1);
+                }
+            });
+
+            man_view_class.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, ClassListActivity.class);
+                    intent.putExtras(bundle2);
+                    startActivity(intent);
+                }
+            });
+
+            man_view_appoint.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, ReLiActivity.class);
+                    intent.putExtras(bundle2);
+                    startActivity(intent);
+                }
+            });
+
+            man_passchange.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, PassActivity.class);
+                    intent.putExtras(bundle2);
+                    startActivityForResult(intent, 1);
+                }
+            });
+        }
+    }
+}
